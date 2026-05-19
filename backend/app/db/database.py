2b6351e1e71867c1d8f35115294ea598f2fd98af
Iverson Culanag <iverson.culanag21@gmail.com>
2026-05-19 08:25:56 +0800

    inayos db name

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,11 +1,9 @@
 import psycopg2
+
 from app.core.config import settings
+
 
 def get_connection():
     return psycopg2.connect(
-        host=settings.DB_HOST,
-        database=settings.DB_NAME,
-        user=settings.DB_USER,
-        password=settings.DB_PASSWORD,
-        port=settings.DB_PORT
+        settings.DB_URL
     )
