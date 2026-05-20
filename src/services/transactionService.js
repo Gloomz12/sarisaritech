930d716092787e0c900ccc1fa25b39e92ec9938e
Iverson Culanag <iverson.culanag21@gmail.com>
2026-05-21 03:59:28 +0800

    fix routing sa transaction

--- src/services/transactionService.js
+++ src/services/transactionService.js
@@ -3,13 +3,13 @@ import api from "./api";
 const transactionService = {
   // GET ALL TRANSACTIONS
   getAllTransactions: async () => {
-    const response = await api.get("/transactions");
+    const response = await api.get("/transactions/");
     return response.data;
   },
 
   // CREATE TRANSACTION
   createTransaction: async (data) => {
-    const response = await api.post("/transactions", data);
+    const response = await api.post("/transactions/", data);
     return response.data;
   },
 };
